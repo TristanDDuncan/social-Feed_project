eddd0f081e59cb0010e572a341405453b5b99bbc
TristanDDuncan <125073354+TristanDDuncan@users.noreply.github.com>
2023-04-12 13:18:28 -0400

    Add files via upload

---
+++ src/Components/CustomButton/CustomButton.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import "./CustomButton.css"
+
+
+export default CustomButton
---
+++ src/Components/NavBar/navBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">SocialFeed</h1>
+    </nav>
+  );
+};
+
+export default NavBar;
+
